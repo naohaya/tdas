4658efebdd7ae6455c81fb503d78cac218d6c64b
林原 尚浩 <naohaya@cajon.local>
2019-10-08 17:41:21 +0900

    added Data.java

---
+++ Data.java
@@ -0,0 +1,17 @@
+
+public class Data
+{
+	private String data;
+
+	public Data(String data) {
+		this.setData(data);
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getData() {
+		return this.data;
+	}
+}
